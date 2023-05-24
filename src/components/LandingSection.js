cf9447e30d373157c0787ed808b2520138a9c61e
curtawhelan <curtawhelan@outlook.com>
2023-05-24 16:19:59 -0400

    added home button and updated with personal info for landing page

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -2,7 +2,7 @@ import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 import FullScreenSection from "./FullScreenSection";
 
-const greeting = "Hello, I am Pete!";
+const greeting = "Hello, I am Curt Whelan!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
 
@@ -14,10 +14,15 @@ const LandingSection = () => (
     alignItems="center"
     isDarkBackground
     backgroundColor="#2A4365"
+    id="landing-section"
   >
     <VStack>
-      <Avatar size="2xl" src="https://i.pravatar.cc/150?img=7"></Avatar>
-      <Heading as="h3" size="sm" paddingBottom="40px">
+      <Avatar
+        size="2xl"
+        src="https://media.licdn.com/dms/image/D5635AQFMjXzYQw6JpA/profile-framedphoto-shrink_400_400/0/1680128313718?e=1685566800&v=beta&t=a0OCrGi5ahgyJw9kUMd64mWDkwfo_BcoYHk19aBjw3M"
+        alt="Picture of Curt Whelan"
+      ></Avatar>
+      <Heading as="h3" size="sm" paddingBottom={"40px"}>
         {greeting}
       </Heading>
       <Heading>{bio1}</Heading>
